42d685e424f125cda1aa47cb1a1020993d47f5cb
Gustavo <gustavogarciap2506@gmail.com>
2026-06-13 15:40:00 -0500

    Se crean el módulo servicios y sus dependecias en el pom

---
+++ VetCitasServicios/src/main/java/pe/edu/pucp/vetcitas/servicios/rest/RestApplication.java
@@ -0,0 +1,8 @@
+package pe.edu.pucp.vetcitas.servicios.rest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("webresources")
+public class RestApplication extends Application {
+}
